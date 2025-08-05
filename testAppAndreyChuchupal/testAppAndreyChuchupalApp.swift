00b6debc9c8ef32a5749e5e9941e3edb751ff9a0
JLNap <chuchupal@bk.ru>
2025-08-05 15:43:43 +0300

    Initial Commit

---
+++ testAppAndreyChuchupal/testAppAndreyChuchupalApp.swift
@@ -0,0 +1,17 @@
+//
+//  testAppAndreyChuchupalApp.swift
+//  testAppAndreyChuchupal
+//
+//  Created by Андрей Чучупал on 05.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct testAppAndreyChuchupalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
